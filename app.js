bbeb6c360c959982d7cd3000a66e20cf34e11a2d
Tony Blair <blairt371.dev@gmail.com>
2019-05-12 13:25:28 +0300

    Sets up graphql resolvers

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema} = require('graphql');
+
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello Tony');
+app.use('/graphql',
+ graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+             query: RootQuery
+             mutation: RootMutation
+        }
+        
+   `),
+   // bundles up all our resolvers
+    rootValue: {
+        events: () => {
+            return ['I Love Dancing', 'Travelling', 'Watching Football'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true      
 })
+);
+
+// app.get('/', (req, res, next) => {
+//     res.send('Hello Tony');
+// })
 app.listen(4000);
