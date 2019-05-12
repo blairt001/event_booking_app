6e37fff47a3f82e4d8de0ee4c95584eb9af4cb22
Tony Blair <blairt371.dev@gmail.com>
2019-05-12 14:02:31 +0300

    Create graphql events

--- app.js
+++ app.js
@@ -5,19 +5,35 @@ const { buildSchema} = require('graphql');
 
 
 const app = express();
+const events = [];
 
 app.use(bodyParser.json());
 
 app.use('/graphql',
  graphqlHttp({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
 
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -29,11 +45,22 @@ app.use('/graphql',
    // bundles up all our resolvers
     rootValue: {
         events: () => {
-            return ['I Love Dancing', 'Travelling', 'Watching Football'];
+            // return ['I Love Dancing', 'Travelling', 'Watching Football'];
+            return event;
         },
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            // const eventName = args.name;
+            // return eventName;
+            const event = {
+                _id: Math.random().toString(),
+                title:args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+               //  date: new Date().toISOString()
+               date: args.eventInput.date
+            }
+            events.push(event);
+            return event;
         }
     },
     graphiql: true      
